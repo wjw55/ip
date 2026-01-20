82d9679c79138bbf83ba174499236f13caa2bf32
wjw55 <wjw50100@gmail.com>
2026-01-20 20:28:58 +0800

    Level-0: Rename, Greet, Exit

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# John User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/John.java
@@ -0,0 +1,8 @@
+public class John {
+    public static void main(String[] args) {
+        String name = "John";
+        System.out.println("Hello! I'm " + name);
+        System.out.println("What can I do for you?\n" + "\n"+ "Bye. Hope to see you again soon!");
+
+    }
+}
