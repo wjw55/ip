c39ee17ebb0a235df9ce04e99c6eee43110ead6d
wjw55 <wjw50100@gmail.com>
2026-01-27 21:26:00 +0800

    Complete Level -1

--- src/main/java/John.java
+++ src/main/java/John.java
@@ -1,8 +1,18 @@
+import java.util.Scanner;
+
 public class John {
     public static void main(String[] args) {
         String name = "John";
         System.out.println("Hello! I'm " + name);
-        System.out.println("What can I do for you?\n" + "\n"+ "Bye. Hope to see you again soon!");
+        System.out.println("What can I do for you?");
+        String line;
+        Scanner in = new Scanner(System.in);
+        line = in.nextLine();
+        while(!line.equals("bye")){
+            System.out.println(line);
+            line = in.nextLine();
+        }
+        System.out.println("Bye. Hope to see you again soon!");
 
     }
 }
