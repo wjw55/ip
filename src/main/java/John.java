5d612da6b7a8f83bf348ffb9ec4aecfbd81c8a44
wjw55 <wjw50100@gmail.com>
2026-01-27 22:54:05 +0800

    Complete Level-3

--- src/main/java/John.java
+++ src/main/java/John.java
@@ -8,17 +8,32 @@ public class John {
         String line;
         Scanner in = new Scanner(System.in);
         line = in.nextLine();
-        String[] list = new String[100];
+        Task[] list = new Task[100];
         int count = 0;
         while(!line.equals("bye")){
+            String[] words = line.split(" ");
             if (line.equals("list")) {
+                System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < count; i++){
                     int num = i + 1;
-                    System.out.println(num + ". " + list[i]);
+                    System.out.println(num + "." +'[' + list[i].getStatusIcon() + "] "+ list[i].description);
                 }
             }
+            else if (words[0].equals("mark")) {
+                int item = Integer.parseInt(words[1]) - 1;
+                list[item].markAsDone();
+                System.out.println("Nice! I've marked this task as done: ");
+                System.out.println(list[item]);
+            }
+            else if (words[0].equals("unmark")) {
+                int item = Integer.parseInt(words[1]) - 1;
+                list[item].markAsUndone();
+                System.out.println("OK, I've marked this task as not done yet: ");
+                System.out.println(list[item]);
+            }
             else {
-                list[count] = line;
+                Task t = new Task(line);
+                list[count] = t;
                 count++;
                 System.out.println("added: " + line);
             }
