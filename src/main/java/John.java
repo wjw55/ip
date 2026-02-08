edcc8b5f6ff304a8ab3f498461a5d78144ee5d34
wjw55 <wjw50100@gmail.com>
2026-02-08 21:05:09 +0800

    Add validation for all task types and command arguments

--- src/main/java/John.java
+++ src/main/java/John.java
@@ -13,7 +13,6 @@ public class John {
     }
 
     public static void main(String[] args) throws DukeException {
-
         String name = "John";
         System.out.println("Hello! I'm " + name);
         System.out.println("What can I do for you?");
@@ -33,30 +32,48 @@ public class John {
                         System.out.println(num + "." + Tasks[i].toString());
                     }
                 } else if (words[0].equals("mark")) {
+                    if (words.length < 2) {
+                        throw new DukeException("Please specify which task you want to mark.");
+                    }
                     int item = Integer.parseInt(words[1]) - 1;
+                    if (item > taskCount - 1) {
+                        throw new DukeException("This task does not exist.");
+                    }
                     Tasks[item].markAsDone();
                     System.out.println("Nice! I've marked this task as done: ");
                     System.out.println(Tasks[item]);
                 } else if (words[0].equals("unmark")) {
+                    if (words.length < 2) {
+                        throw new DukeException("Please specify which task you want to unmark.");
+                    }
                     int item = Integer.parseInt(words[1]) - 1;
+                    if (item > taskCount - 1) {
+                        throw new DukeException("This task does not exist.");
+                    }
                     Tasks[item].markAsUndone();
                     System.out.println("OK, I've marked this task as not done yet: ");
                     System.out.println(Tasks[item]);
                 } else if (words[0].equals("todo")) {
                     if (words.length < 2) {
-                        throw new DukeException("OOPS! Seems like you did not enter a description for todo ;-;");
+                        throw new DukeException("OOPS! Seems like you did not enter a description for todo ;-; Please enter a todo with description");
                     }
                     int firstSpace = line.indexOf(" ");
                     String description = line.substring(firstSpace + 1);
                     addTask(new Todo(description));
                 } else if (words[0].equals("deadline")) {
                     int firstSpace = line.indexOf(" ");
+                    if (!line.contains("/by ")) {
+                        throw new DukeException("Please enter deadline in the correct format (with /by).");
+                    }
                     int byIndex = line.indexOf("/by ");
                     String description = line.substring(firstSpace + 1, byIndex);
                     String by = line.substring(byIndex + 1).replace("by ", "");
                     addTask(new Deadline(description, by));
                 } else if (words[0].equals("event")) {
                     int firstSpace = line.indexOf(" ");
+                    if (!line.contains("/from") || (!line.contains("/to"))) {
+                        throw new DukeException("Please enter event in the correct format (with /from and /to).");
+                    }
                     int fromIndex = line.indexOf("/from");
                     int toIndex = line.indexOf("/to");
                     String description = line.substring(firstSpace + 1, fromIndex);
@@ -64,7 +81,7 @@ public class John {
                     String endTime = line.substring(toIndex + 1).replace("to ", "");
                     addTask(new Event(description, startTime, endTime));
                 } else {
-                    throw new DukeException("OOPS! I'm sorry but I dont understand this :( Please enter an valid task");
+                    throw new DukeException("OOPS! Sorry I don't understand this :(");
                 }
             } catch (DukeException e) {
                 System.out.println(e.getMessage());
